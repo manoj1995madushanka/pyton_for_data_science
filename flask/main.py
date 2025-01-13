ec0e85c39ba6601a7daf88afccf5be2b7797c1e3
manoj1995madushanka <madushankamanoj414@gmail.com>
2025-01-13 23:22:30 +0530

    implement POST api mapping

--- flask/main.py
+++ flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # create an instance of the flask class
 # which will be your WSGI application
@@ -10,9 +10,23 @@ def welcome():
     return "<html><H1>Welcome to the flask course</H1></html>"
 
 
-@app.route("/index")
+@app.route("/indexold")
+def indexOld():
+    return render_template('index.html')
+
+
+@app.route("/index", methods=['GET'])
 def index():
     return render_template('index.html')
+
+
+@app.route("/form", methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f'Hello {name}'
+        pass
+    return render_template('form.html')
 
 
 if __name__ == "__main__":
